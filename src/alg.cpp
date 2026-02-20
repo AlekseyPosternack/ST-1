43c18f0e38590bd37943bc3d96eae96477cef14e
Alexey Posternak <lockharn@student.21-school.ru>
2026-02-20 15:41:23 +0300

    first_commit

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,54 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+  bool is_prime = true;
+
+  if (value == 0 || value == 1) {
+    is_prime = false;
+  }
+
+  for (uint64_t i = 2; i * i <= value; ++i) {
+    if (value % i == 0) {
+      is_prime = false;
+      break;
+    }
+  }
+  return is_prime;
 }
 
 uint64_t nPrime(uint64_t n) {
   // вставьте код функции
-  return 2;
+  if (n == 0) {
+    return 0;
+  }
+
+  uint64_t count = 0;
+  uint64_t n_prime = 1;
+  while (count != n) {
+    n_prime++;
+    if (checkPrime(n_prime)) {
+      count++;
+    }
+  }
+  return n_prime;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t next_prime = value;
+  while (true) {
+    next_prime++;
+    if(checkPrime(next_prime)) {
+      return next_prime;
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -15,3 +15,49 @@ TEST(st1, sumPrime2) {
   uint64_t expected = 17;
   EXPECT_EQ(expected, res);
 }
+
+// CheckPrime функциональные тесты
+TEST(st1, checkPrime_functional) {
+  EXPECT_TRUE(checkPrime(97));
+  EXPECT_FALSE(checkPrime(100));
+  EXPECT_TRUE(checkPrime(101));
+  EXPECT_FALSE(checkPrime(121)); 
+}
+
+// CheckPrime граничные тесты
+TEST(st1, checkPrime_range) {
+  EXPECT_FALSE(checkPrime(0));
+  EXPECT_FALSE(checkPrime(1));
+  EXPECT_TRUE(checkPrime(2));
+  EXPECT_TRUE(checkPrime(3));
+  EXPECT_FALSE(checkPrime(4));
+}
+
+// nPrime тесты
+TEST(st1, nPrime) {
+  EXPECT_EQ(nPrime(0), 0);
+  EXPECT_EQ(nPrime(1), 2);
+  EXPECT_EQ(nPrime(2), 3);
+  EXPECT_EQ(nPrime(3), 5);
+  EXPECT_EQ(nPrime(25), 97);
+  EXPECT_EQ(nPrime(26), 101);
+}
+
+// nextPrime тесты
+TEST(st1, nextPrime) {
+  EXPECT_EQ(nextPrime(0), 2);
+  EXPECT_EQ(nextPrime(1), 2);
+  EXPECT_EQ(nextPrime(2), 3);
+  EXPECT_EQ(nextPrime(3), 5);
+  EXPECT_EQ(nextPrime(25), 29);
+  EXPECT_EQ(nextPrime(26), 29);
+  EXPECT_EQ(nextPrime(100), 101);
+}
+
+// sumPrime граничные тесты
+TEST(st1, sumPrime_range) {
+  EXPECT_EQ(sumPrime(0), 0);
+  EXPECT_EQ(sumPrime(1), 0);
+  EXPECT_EQ(sumPrime(2), 0);
+  EXPECT_EQ(sumPrime(3), 2);
+}
