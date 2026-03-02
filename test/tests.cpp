7a9544b3339035559625ded29d2d9883c3d46b5f
Alexey Posternak <lockharn@student.21-school.ru>
2026-03-02 11:46:06 +0300

    test_fixes

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,12 +4,12 @@
 #include <cstdint>
 #include "alg.h"
 
-
 TEST(st1, sumPrime1) {
   uint64_t res = sumPrime(2000000);
   uint64_t expected = 142913828922;
   EXPECT_EQ(expected, res);
 }
+
 TEST(st1, sumPrime2) {
   uint64_t res = sumPrime(10);
   uint64_t expected = 17;
@@ -17,47 +17,144 @@ TEST(st1, sumPrime2) {
 }
 
 // CheckPrime функциональные тесты
-TEST(st1, checkPrime_functional) {
+TEST(st1, checkPrime_functional1) {
   EXPECT_TRUE(checkPrime(97));
+}
+
+TEST(st1, checkPrime_functional2) {
   EXPECT_FALSE(checkPrime(100));
+}
+
+TEST(st1, checkPrime_functional3) {
   EXPECT_TRUE(checkPrime(101));
-  EXPECT_FALSE(checkPrime(121));
+}
+
+TEST(st1, checkPrime_functional4) {
+  EXPECT_FALSE(checkPrime(121)); 
 }
 
 // CheckPrime граничные тесты
-TEST(st1, checkPrime_range) {
+TEST(st1, checkPrime_range1) {
   EXPECT_FALSE(checkPrime(0));
+}
+
+TEST(st1, checkPrime_range2) {
   EXPECT_FALSE(checkPrime(1));
+}
+
+TEST(st1, checkPrime_range3) {
   EXPECT_TRUE(checkPrime(2));
+}
+
+TEST(st1, checkPrime_range4) {
   EXPECT_TRUE(checkPrime(3));
+}
+
+TEST(st1, checkPrime_range5) {
   EXPECT_FALSE(checkPrime(4));
 }
 
 // nPrime тесты
-TEST(st1, nPrime) {
-  EXPECT_EQ(nPrime(0), 0);
-  EXPECT_EQ(nPrime(1), 2);
-  EXPECT_EQ(nPrime(2), 3);
-  EXPECT_EQ(nPrime(3), 5);
-  EXPECT_EQ(nPrime(25), 97);
-  EXPECT_EQ(nPrime(26), 101);
+TEST(st1, nPrime1) {
+  uint64_t res = nPrime(0);
+  uint64_t expected = 0;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nPrime2) {
+  uint64_t res = nPrime(1);
+  uint64_t expected = 2;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nPrime3) {
+  uint64_t res = nPrime(2);
+  uint64_t expected = 3;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nPrime4) {
+  uint64_t res = nPrime(3);
+  uint64_t expected = 5;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nPrime5) {
+  uint64_t res = nPrime(25);
+  uint64_t expected = 97;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nPrime6) {
+  uint64_t res = nPrime(26);
+  uint64_t expected = 101;
+  EXPECT_EQ(expected, res);
 }
 
 // nextPrime тесты
-TEST(st1, nextPrime) {
-  EXPECT_EQ(nextPrime(0), 2);
-  EXPECT_EQ(nextPrime(1), 2);
-  EXPECT_EQ(nextPrime(2), 3);
-  EXPECT_EQ(nextPrime(3), 5);
-  EXPECT_EQ(nextPrime(25), 29);
-  EXPECT_EQ(nextPrime(26), 29);
-  EXPECT_EQ(nextPrime(100), 101);
+TEST(st1, nextPrime1) {
+  uint64_t res = nextPrime(0);
+  uint64_t expected = 2;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nextPrime2) {
+  uint64_t res = nextPrime(1);
+  uint64_t expected = 2;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nextPrime3) {
+  uint64_t res = nextPrime(2);
+  uint64_t expected = 3;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nextPrime4) {
+  uint64_t res = nextPrime(3);
+  uint64_t expected = 5;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nextPrime5) {
+  uint64_t res = nextPrime(25);
+  uint64_t expected = 29;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nextPrime6) {
+  uint64_t res = nextPrime(26);
+  uint64_t expected = 29;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, nextPrime7) {
+  uint64_t res = nextPrime(100);
+  uint64_t expected = 101;
+  EXPECT_EQ(expected, res);
 }
 
 // sumPrime граничные тесты
-TEST(st1, sumPrime_range) {
-  EXPECT_EQ(sumPrime(0), 0);
-  EXPECT_EQ(sumPrime(1), 0);
-  EXPECT_EQ(sumPrime(2), 0);
-  EXPECT_EQ(sumPrime(3), 2);
+TEST(st1, sumPrime_range1) {
+  uint64_t res = sumPrime(0);
+  uint64_t expected = 0;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, sumPrime_range2) {
+  uint64_t res = sumPrime(1);
+  uint64_t expected = 0;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, sumPrime_range3) {
+  uint64_t res = sumPrime(2);
+  uint64_t expected = 0;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1, sumPrime_range4) {
+  uint64_t res = sumPrime(3);
+  uint64_t expected = 2;
+  EXPECT_EQ(expected, res);
 }
